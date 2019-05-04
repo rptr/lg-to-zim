0b54b1f4af09a3594e9ae9ef8b5bf17b851a7a5b
rptr <wasmus@zom.bi>
2019-05-04 16:08:23 +0200

    add 1050 loading

--- lg2zim.py
+++ lg2zim.py
@@ -62,15 +62,14 @@ def convert_lg_diary (filepath):
 
     version = fd.readline()
     vnum = int(version.split(' ')[1])
+    fd.close()
 
     if vnum == 1050 or vnum == 1040 or vnum == 1030 or vnum == 1020:
         v1050.load(filepath)
     elif vnum == 1011 or vnum == 1010:
         read_diary_1010(fd)
-    else
+    else:
         read_diary_110(fd)
-
-    fd.close()
 
 if len(sys.argv) > 1:
     convert_lg_diary(sys.argv[1])
