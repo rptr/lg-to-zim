f505c50eb249f88d1198683a69e0073ca517c2f6
rptr <wasmus@zom.bi>
2019-05-04 16:41:55 +0200

    update gitignore

--- lg2zim.py
+++ lg2zim.py
@@ -1,57 +1,26 @@
 import sys
 import os
-import v1050, v1010, v110
+import v1050, v1010, v110, zim
 
 LG_VERSION  = 1050
-
-#
-# ZIM GENERATION
-#
-
-def zim_header ():
-    return "Content-Type: text/x-zim-wiki\nWiki-Format: zim 0.4\nCreation-Date: 2019-05-04T10:45:10+02:00"
-
-print zim_header()
-
-def zim_generate ():
-    pass
-
-def zim_make_chapter (cid):
-    pass
-
-def zim_make_entry ():
-    pass
 
 chapters = {}
 entries  = {}
 
-#
 # 1010
-#
-
 def read_diary_1010 (fd):
     print "version 1010 is not supported"
 
-#
 # 110
-#
-
 def read_diary_110 (fd):
     print "version 110 is not supported"
 
 def convert_lg_diary (filepath):
-    diary_name = filepath.split('.')[0] + "_zim"
+    diary_name = filepath.split('.')[0]
 
     if os.path.exists(diary_name):
         print diary_name, "already exists"
 #        return
-
-    try:
-        os.rmdir(diary_name)
-    except OSError:
-        pass
-
-    os.mkdir(diary_name)
 
     fd = open(filepath)
     line = fd.readline()
@@ -65,11 +34,13 @@ def convert_lg_diary (filepath):
     fd.close()
 
     if vnum == 1050 or vnum == 1040 or vnum == 1030 or vnum == 1020:
-        v1050.load(filepath)
+        chapters, entries = v1050.load(filepath)
     elif vnum == 1011 or vnum == 1010:
         read_diary_1010(fd)
     else:
         read_diary_110(fd)
+
+    zim.write(diary_name, chapters, entries)
 
 if len(sys.argv) > 1:
     convert_lg_diary(sys.argv[1])
